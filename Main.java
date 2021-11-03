f79140efeed9d2739f20a3d7ff4b74dd8c48dc7b
Erin Drew <93667936+ErinDrew@users.noreply.github.com>
2021-11-03 16:25:14 +0000

    Added scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    Scanner the = new Scanner(System.in);
     System.out.println("Hello world!");
+    String hi = the.nextLine();
+    System.out.println("hi");
   }
 }
